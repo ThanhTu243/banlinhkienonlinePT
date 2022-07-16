ff7f63e3ef04d18dbdb5cdb9e082328aaa65e03a
nthanhtu243 <nthanhtu243@gmail.com>
2022-07-16 16:51:02 +0700

    fix search admin

--- src/main/java/com/thanhtu/crud/controller/admin/ProductManagementController.java
+++ src/main/java/com/thanhtu/crud/controller/admin/ProductManagementController.java
@@ -152,7 +152,7 @@ public class ProductManagementController {
         }
         else if(category ==null && supplier==null && keyword != null)
         {
-            Page<ProductEntity> productByKeyWordList=productService.getListProductByKeyWord1(keyword,pageable);
+            Page<ProductEntity> productByKeyWordList=productService.getListProductByKeyWord1Admin(keyword,pageable);
             totalPages=productByKeyWordList.getTotalPages();
             currentPage=productByKeyWordList.getNumber()+1;
             listPro=productByKeyWordList.toList();
